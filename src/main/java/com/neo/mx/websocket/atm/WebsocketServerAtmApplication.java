07965d9c3b5e4f6c83ac685fee0d5e1dd68dafcc
cesardev <xxcessarxx@gmail.com>
2020-03-04 23:59:34 -0600

    Commit de inicialización

---
+++ src/main/java/com/neo/mx/websocket/atm/WebsocketServerAtmApplication.java
@@ -0,0 +1,13 @@
+package com.neo.mx.websocket.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketServerAtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketServerAtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo/mx/websocket/atm/WebsocketServerAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.mx.websocket.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketServerAtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
